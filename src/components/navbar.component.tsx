cf169470c8f89402c0fef89d38757b794ed37777
michelXCVI <mcharl05@nyit.edu>
2020-07-11 00:52:00 -0400

    2nd essai

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fade, makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
@@ -16,8 +16,6 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import MoreIcon from '@material-ui/icons/MoreVert';
 import './navbar.component.css';
 import '../logo/image.png'
-import Image from "material-ui-image";
-import Avatar from '@material-ui/core/Avatar'
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
@@ -201,7 +199,7 @@ export const NavbarComponent: React.FC = () => {
 
             <div  className={classes.search}>
               <div className={classes.searchIcon}>
-                <SearchIcon  id="search-bar"/>
+                <SearchIcon  id="search-icon"/>
               </div>
               <InputBase
                 
