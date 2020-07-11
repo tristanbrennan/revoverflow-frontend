af822f853e6878a4d2a6ba7f335c5b37888b7934
michelXCVI <mcharl05@nyit.edu>
2020-07-11 01:06:51 -0400

    prepped login to merge

--- src/components/pages/login.component/login.component.tsx
+++ src/components/pages/login.component/login.component.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import './login.component.css';
-import { orange } from '@material-ui/core/colors';
+
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
             '& .MuiTextField-root': {
                 margin: theme.spacing(1),
                 width: '100ch',
-                color: 'orangered'
             },
         },
     }),
@@ -24,8 +23,8 @@ export const LoginComponent: React.FC = () =>{
                     Login
                     </h3>
         <form className={classes.root} noValidate autoComplete="off">
-            <TextField id="outlined-basic" label="Email" variant="outlined" className="loginUsername"/>
-            <TextField id="outlined-basic" label="Password" type="password" variant="outlined" className="loginPassword"/>
+            <TextField id="outlined-basic" label="Email" type="email" variant="outlined" />
+            <TextField id="outlined-basic" label="Password" type="password" variant="outlined" />
         </form>
         <div className="logIn">
             <button type="submit" >Log In</button>
