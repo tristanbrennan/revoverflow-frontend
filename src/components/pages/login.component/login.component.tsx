062dcf684bab095aa15bbd4468dbbfe3f607a651
michelXCVI <mcharl05@nyit.edu>
2020-07-15 16:02:07 -0400

    connecting login with server side

--- src/components/pages/login.component/login.component.tsx
+++ src/components/pages/login.component/login.component.tsx
@@ -3,10 +3,13 @@
  * @author Michel Charles <mcharl05@nyit.edu>
  */
 
-import React from "react";
+import React, { useEffect } from "react";
 import TextField from "@material-ui/core/TextField";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import "./login.component.css";
+import * as loginRemote from '../../../remotes/login.remote'
+import { useHistory } from 'react-router';
+import { useState } from 'react';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -21,6 +24,49 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export const LoginComponent: React.FC = () => {
   const classes = useStyles();
+
+  const history = useHistory();
+  const [inputEmail, setInputEmail] = useState('');
+  const [inputPassword, setInputPassword] = useState('');
+
+  useEffect(() => {}, []);
+
+let response: any;
+const setInformation = async()=>{
+  setInputEmail('');
+  setInputPassword('');
+  console.log(response)
+  const authToken=response.data.token;
+  //const decodeValue= JSON.parse(window.atob(authToken.split('.')[1]))
+  localStorage.setItem('accessToken', authToken);
+  localStorage.setItem('admin', authToken);
+  localStorage.setItem('email', authToken)
+  localStorage.setItem('firstName', authToken);
+  localStorage.setItem('lastName', authToken);
+  localStorage.setItem('jwt', authToken);
+  localStorage.setItem('points', authToken);
+  localStorage.setItem('profilePicture', authToken);
+  localStorage.setItem('rssaccountId', authToken);
+  localStorage.setItem('userID', authToken);
+  history.push('/feed')
+}
+
+const addLoginCredentials = async () => {
+  const payload = {
+    email: inputEmail,
+    password: inputPassword
+  };
+      try {
+            response = await loginRemote.checkLoginCredentials(payload);
+            await setInformation();
+        } catch { 
+            alert('Incorrect username and/or password')
+            }
+        
+        } 
+
+
+  
   return (
     <div>
       <img alt="logo" id="logo" src={require("../../../logo/image.png")} />
@@ -34,16 +80,20 @@ export const LoginComponent: React.FC = () => {
               label="Email"
               type="email"
               variant="outlined"
+              value={inputEmail}
+              onChange={(e) => setInputEmail(e.target.value)}
             />
             <TextField
               id="outlined-basic"
               label="Password"
               type="password"
               variant="outlined"
+              value={inputPassword}
+              onChange={(e) => setInputPassword(e.target.value)}
             />
           </form>
           <div className="logIn">
-            <button type="submit">Log In</button>
+            <button type="submit" onClick={() => addLoginCredentials()}>Log In</button>
           </div>
         </div>
       </div>
