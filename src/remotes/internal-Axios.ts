d739b07a8cf5d88601da7fa6b253647d0f4a38ae
Kasshern <1400536468@qq.com>
2020-07-16 12:23:12 -0400

    cut out ternary operator from internal axios

--- src/remotes/internal-Axios.ts
+++ src/remotes/internal-Axios.ts
@@ -5,3 +5,10 @@ const server = "http://localhost:8080";
 export const internalAxios = Axios.create({
     baseURL: server,
 });
+
+export const authAxios = Axios.create({
+    baseURL: server,
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    },
+});
