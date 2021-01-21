b40ee0000998aa966f54ed3647ce5f1bfeed27f7
Stephen Razis <srazis@yahoo.com>
2021-01-21 10:03:39 -0800

    stubbed tests

---
+++ src/__tests__/components/location-filter.component.test.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { mount } from 'enzyme';
+
+describe('location-filter.component', () => {
+
+    it('should mount', () => {
+        
+        const wrapper = mount(<LocationFilter />);
+        expect(true).toBeTruthy();
+    })
+
+    // TODO
+    it('should display display a drop-down list when the control is clicked', () => {
+        
+        const wrapper = mount(<LocationFilter />);
+        expect(true).toBeTruthy();
+    })
+
+    // TODO
+    it('should display display a drop-down list with the expected values', () => {
+        
+        const wrapper = mount(<LocationFilter />);
+        expect(true).toBeTruthy();
+    })
+
+    // TODO
+    it('should close when clicking outside of it', () => {
+        
+        const wrapper = mount(<LocationFilter />);
+        expect(true).toBeTruthy();
+    })
+
+    // TODO
+    it('should close when the icon is clicked and it is already open', () => {
+        
+        const wrapper = mount(<LocationFilter />);
+        expect(true).toBeTruthy();
+    })
+
+    // TODO
+    it('should retrieve questions by the location value that is clicked in the drop-down', () => {
+        
+        const wrapper = mount(<LocationFilter />);
+        expect(true).toBeTruthy();
+    })
+
+    // TODO
+    it('should display the filtered questions by the location value that is clicked in the drop-down', () => {
+        
+        const wrapper = mount(<LocationFilter />);
+        expect(true).toBeTruthy();
+    })
+
+    // TODO
+    it('should update the filtered questions state when the filter option is clicked', () => {
+        
+        const wrapper = mount(<LocationFilter />);
+        expect(true).toBeTruthy();
+    })
+});
