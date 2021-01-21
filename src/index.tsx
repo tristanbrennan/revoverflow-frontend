6b5b8a174e4347f3dd618d6da44ad2913d21e08d
Antardeep <antardeep.kaur.sidhu@gmail.com>
2021-01-21 14:21:21 -0500

    initial firebase setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from "firebase/app";
+import "firebase/auth";
+import {FirebaseAuthProvider} from '@react-firebase/auth';
+
+
+const config = {
+  apiKey: "AIzaSyC4sxZlT-McTildwtxa8LV1lj7ZQhzOrs0",
+  authDomain: "training-team-253916.firebaseapp.com",
+  projectId: "training-team-253916",
+  databaseURL: "",
+  storageBucket: "training-team-253916.appspot.com",
+  messagingSenderId: "492701958610",
+  appId: "1:492701958610:web:4a30a1be93803701d3480b",
+  measurementId: "G-DP6XDH9DTW"
+};
 
 ReactDOM.render(
+  <FirebaseAuthProvider {...config} firebase={firebase}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </FirebaseAuthProvider>,
   document.getElementById('root')
 );
 
